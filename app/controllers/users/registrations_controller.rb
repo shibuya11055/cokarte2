a88fd98f1bc445c78918dcf587d1fcb225789276
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-08-31 22:46:12 +0900

    ログイン画面を用意

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,8 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  # 登録後はログイン画面にリダイレクト
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
+end
