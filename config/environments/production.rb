085a66bce248f5fdc2088285543219dc53758f7d
澁谷恭平 <shibuyakyouhei@kyohei-shibuya.local>
2025-09-04 19:57:39 +0900

    メール設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
+  config.require_master_key = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
@@ -65,7 +67,7 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            Rails.application.credentials.dig(:gmail, :user_name),
+    user_name:            Rails.application.credentials.dig(:gmail, :email),
     password:             Rails.application.credentials.dig(:gmail, :app_password),
     authentication:       "plain",
     enable_starttls_auto: true
